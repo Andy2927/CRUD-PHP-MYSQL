e06c40003f1a9b0a0c23ee0f01be3b4eafaf4f0d
Andrea Nieto Rodríguez <nietorodriguezandrea@gmail.com>
2024-01-27 17:52:39 +0100

    correccion de archivos

--- disney.sql
+++ disney.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 05-12-2012 a las 11:18:25
--- Versión del servidor: 5.5.28
--- Versión de PHP: 5.4.6-1ubuntu1.1
+-- Servidor: db
+-- Tiempo de generación: 27-01-2024 a las 16:47:06
+-- Versión del servidor: 8.2.0
+-- Versión de PHP: 8.2.8
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `disney`
@@ -23,39 +24,68 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `personaje`
---
-
-CREATE TABLE IF NOT EXISTS `personaje` (
-  `id_Personaje` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
-  `pelicula` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
-  `actorDoblaje` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
-  PRIMARY KEY (`id_Personaje`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `cliente`
---
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `pelicula`
 --
 
-CREATE TABLE IF NOT EXISTS `pelicula` (
-  `id_Pelicula` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
-  `año` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
-  `puntuacion` double NOT NULL,
-  PRIMARY KEY (`id_Pelicula`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+CREATE TABLE `pelicula` (
+  `id_Pelicula` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `nombre` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `año` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `puntuacion` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `pelicula`
 --
 
+INSERT INTO `pelicula` (`id_Pelicula`, `nombre`, `año`, `puntuacion`) VALUES
+('01', 'Cars', '2006', 10),
+('02', 'La bella durmiente', '1959', 6.8),
+('03', 'Blancanieves y los siete enanitos', '1937', 7.5),
+('04', 'Toy Story', '1995', 5),
+('05', 'Piratas del Caribe la maldición de la perla negra', '2003', 10),
+('06', 'Cars 3', '2017', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personaje`
+--
+
+CREATE TABLE `personaje` (
+  `id_Personaje` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `nombre` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `pelicula` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `actorDoblaje` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `personaje`
+--
+
+INSERT INTO `personaje` (`id_Personaje`, `nombre`, `pelicula`, `actorDoblaje`) VALUES
+('01', 'Blancanieves', 'Blancanieves y los siete enanitos', 'Amparo Garrido'),
+('02', 'Rayo McQueen', 'Cars', 'Guillermo Romero'),
+('03', 'Jack Sparrow', 'Piratas del Caribe', 'Luís Posada'),
+('04', 'Buzz Lightyear', 'Toy Story', 'José Luís Gil'),
+('33', 'Asistente Cruz', 'Cars 3', 'Don Fernando Alonso Díaz');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  ADD PRIMARY KEY (`id_Pelicula`);
+
+--
+-- Indices de la tabla `personaje`
+--
+ALTER TABLE `personaje`
+  ADD PRIMARY KEY (`id_Personaje`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
